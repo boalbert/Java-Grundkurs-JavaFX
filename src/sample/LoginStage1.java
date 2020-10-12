d1ee9b161bfca24d52aeeba9679cffe32eae9984
albert.andersson <andersson.albert@gmail.com>
2020-10-12 12:08:44 +0200

    Updated LoginStage1

--- src/sample/LoginStage1.java
+++ src/sample/LoginStage1.java
@@ -4,18 +4,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class LoginStage1 extends Application {
 
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage stage) throws Exception{
 
+        stage.setTitle("Login");
+
+        // Header
         Text tHeader = new Text("Login");
+        // Setting ID for stylesheet
+        tHeader.setId("header-text");
 
+        // Created GridPane
+        GridPane gridPane = new GridPane();
 
+        // Added placeholder text
+        gridPane.add(tHeader,0,0);
 
+        // Creating scene
+        Scene scene = new Scene(gridPane);
+        stage.setScene(scene);
 
+        // Setting stylesheet
+        scene.getStylesheets().add("sample/stylesheet.css");
+
+        stage.show();
 
 
     }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,7 +8,6 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-
     public static void main(String[] args) {
         Save.readFile();
         Customer test = new Customer("Casper","Konyi","casper.konyi@iths.se","casper123");
@@ -19,8 +18,12 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
-
+        // Setting LoginStage1 as first primaryStage
+        LoginStage1 stage1 = new LoginStage1();
+        stage1.start(primaryStage);
+        primaryStage.show();
     }
+
 }
